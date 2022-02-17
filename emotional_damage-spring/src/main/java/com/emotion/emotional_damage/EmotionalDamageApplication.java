7a65b564c5414c7d3f97472d18d3f3cd675854f6
hjk33433 <1774958630@qq.com>
2022-02-17 13:20:29 +0800

    Initial commit

---
+++ emotional_damage-spring/src/main/java/com/emotion/emotional_damage/EmotionalDamageApplication.java
@@ -0,0 +1,13 @@
+package com.emotion.emotional_damage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionalDamageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmotionalDamageApplication.class, args);
+    }
+
+}
---
+++ emotional_damage-spring/src/main/java/com/emotion/emotional_damage/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.emotion.emotional_damage;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EmotionalDamageApplication.class);
+    }
+
+}
