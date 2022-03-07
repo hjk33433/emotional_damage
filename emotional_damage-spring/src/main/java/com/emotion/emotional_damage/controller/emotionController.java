b21d5934b36dc6722619fe0fa2b283fe3fb17dde
hjk33433 <1774958630@qq.com>
2022-03-07 17:16:25 +0800

    添加接口

--- emotional_damage-spring/src/main/java/com/emotion/emotional_damage/controller/emotionController.java
+++ emotional_damage-spring/src/main/java/com/emotion/emotional_damage/controller/emotionController.java
@@ -11,8 +11,9 @@ public class emotionController {
     //TODO 用户ip+ 时间 -> token +文字-> 检查token 分析文字 ->返回建议文字 ->vue页面新建 显示结果
     @PostMapping("/query")
     public String emotion(@RequestParam("queryText") String queryText) {
-
-
+        //TODO 分析文本 关键字 统计
+        //TODO 根据统计 对应 分类关键字
+        //TODO 查询 相关数据
         return "";
     }
 }
